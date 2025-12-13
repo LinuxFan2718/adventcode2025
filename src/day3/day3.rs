2ddcda46a145031884668dba989675e426b02590
Dennis Cahillane <dennis@denniscahillane.info>
2025-12-13 15:38:53 -0500

    scaffolding for day3

---
+++ src/day3/day3.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let path = "src/day3/test.txt";
+    // let path = "src/day3/input.txt";
+    // Open the file
+    let file = File::open(&path)
+        .unwrap_or_else(|e| panic!("Could not open {}: {}", path, e));
+
+    // Wrap it in a buffered reader
+    let reader = io::BufReader::new(file);
+    // Iterate over the lines
+    for line_result in reader.lines() {
+        // First, handle I/O errors
+        let text = match line_result {
+            Ok(t) => t,
+            Err(e) => {
+                eprintln!("Error reading line: {}", e);
+                continue;
+            }
+        };
+        println!("{}", text);
+      }
+      Ok(())
+    }
